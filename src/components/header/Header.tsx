f7088ad9977373dda528d670a1b487d4a0f0bf67
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-26 15:30:30 -0300

    feat: configuração inicial do componente header baseado no portfolio da Lua Octaviano

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+import './Header.css';
+
+function Header() {
+  const [menuState, setMenuState] = useState<boolean>(false);
+
+  function toggleMenu() {
+    setMenuState(!menuState);
+  }
+
+  useEffect(() => {
+    const nav = document.querySelector<HTMLInputElement>('nav');
+
+    if (nav != null) {
+      if (menuState) {
+        nav.style.maxHeight = '100vh';
+      } else {
+        nav.style.maxHeight = '';
+      }
+    }
+  }, [menuState]);
+
+  return (
+    <header>
+      <h1 id='title'>
+        <a href='/'>Guilherme Fernandes</a>
+      </h1>
+
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <div onClick={toggleMenu}>
+          {!menuState ? (
+            <FaBars className='menu_hamburger' />
+          ) : (
+            <FaTimes className='menu_hamburger' />
+          )}
+        </div>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a onClick={toggleMenu} href='#about'>
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a onClick={toggleMenu} href='#projects'>
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={toggleMenu}
+              href='https://www.linkedin.com/in/guilherme-ac-fernandes/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Contato
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

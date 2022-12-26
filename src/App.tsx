df5c928d0a63f87dd66dc6eda11cf53f00540a8e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-26 18:01:43 -0300

    feat: estruturação inicial dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Introducion from './components/introducion/Intro';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
 
 import './App.css';
 
 function App() {
-  return <div className='App'>
-    <Header />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <Header />
+      <main className='main'>
+        <Introducion />
+        <About />
+        <Projects />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

8a0f0769e3cdd0ea2f73cbeb4dd63549c5a1b141
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-27 01:19:42 -0300

    feat: estruturação inicial da seção de projetos

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,3 +1,17 @@
+import projects from '../../data/projects';
+import ProjectDisplay from '../project_display/ProjectDisplay';
+
+import './Projects.css';
+
 export default function Projects() {
-  return <>Projects</>;
+  return (
+    <section>
+      <h3>Projetos</h3>
+      <ul className='project_display_container'>
+        {projects.map((project, index) => (
+          <ProjectDisplay project={project} key={index} />
+        ))}
+      </ul>
+    </section>
+  );
 }

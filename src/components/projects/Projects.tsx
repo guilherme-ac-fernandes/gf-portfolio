3d926d47ac07d101ea3265730e64a7b0d3997460
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-28 14:32:16 -0300

    feat: estruturação da seção de projetos

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,8 +5,8 @@ import './Projects.css';
 
 export default function Projects() {
   return (
-    <section>
-      <h3>Projetos</h3>
+    <section className='project_container'>
+      <h2 className='project_container_title'>Projetos</h2>
       <ul className='project_display_container'>
         {projects.map((project, index) => (
           <ProjectDisplay project={project} key={index} />

4ce1f2bd70606f7e66ef3a2aa53eb4ed3d6508b3
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-28 14:32:01 -0300

    feat: estilização inicial dos cards de projetos

--- src/components/project_display/ProjectDisplay.tsx
+++ src/components/project_display/ProjectDisplay.tsx
@@ -10,23 +10,27 @@ interface ProjectDisplayProps {
 export default function ProjectDisplay({ project }: ProjectDisplayProps) {
   return (
     <div className='project_display'>
-      <p>{project.name}</p>
-      <p>{project.description}</p>
       <img src={project.image} alt={project.name} />
-      <ul>
+      <h3>{project.name}</h3>
+      <aside className='project_display_aside'>
+        <p className={`project_module ${project.module}`}>{project.module}</p>
+        <div>
+          <a href={project.codeUrl} target='_blank' rel='noreferrer'>
+            <FaGithub />
+          </a>
+          {project.deployUrl && (
+            <a href={project.deployUrl} target='_blank' rel='noreferrer'>
+              <FaExternalLinkAlt />
+            </a>
+          )}
+        </div>
+      </aside>
+      <p className='project_display_description'>{project.description}</p>
+      {/* <ul>
         {project.skills.map((skill, index) => (
           <li key={`${project.name}-${index}`}>{skill}</li>
         ))}
-      </ul>
-      <a href={project.codeUrl}>
-        <FaGithub />
-      </a>
-      {project.deployUrl && (
-        <a href={project.deployUrl}>
-          <FaExternalLinkAlt />
-        </a>
-      )}
-      <p>{project.module}</p>
+      </ul> */}
     </div>
   );
 }

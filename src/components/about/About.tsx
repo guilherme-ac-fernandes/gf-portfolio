8e2f7da7d7061e04659572c146299e07d84533b5
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 03:23:38 -0300

    feat: construção da seção de sobre

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,3 +1,20 @@
+import SectionTitle from '../section_title/SectionTitle';
+
+import './About.css';
+
 export default function About() {
-  return <>About</>;
+  return (
+    <section id='about' className='about_container'>
+      <SectionTitle title='Sobre Mim' />
+      <div className='about_container_content'>
+        <img src='./profile/profile.png' alt='Guilherme Fernandes' />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aperiam,
+          qui perferendis, culpa hic necessitatibus consectetur officiis dicta
+          maxime ex reprehenderit soluta, tempora rerum numquam molestias
+          tenetur in blanditiis ad?
+        </p>
+      </div>
+    </section>
+  );
 }

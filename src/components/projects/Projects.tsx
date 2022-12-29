3a5afb62bb05bdb45fadbb74e3d257a16b53265e
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 00:31:04 -0300

    refactor: implementação da seção de titulo

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,16 @@
 import projects from '../../data/projects';
 import ProjectDisplay from '../project_display/ProjectDisplay';
+import SectionTitle from '../section_title/SectionTitle';
 
 import './Projects.css';
 
 export default function Projects() {
   return (
-    <section className='project_container'>
-      <h2 className='project_container_title'>Projetos</h2>
+    <section id='projects' className='project_container'>
+      <SectionTitle
+        title='Projetos'
+        text='Confira alguns dos meus projetos recentes'
+      />
       <ul className='project_display_container'>
         {projects.map((project, index) => (
           <ProjectDisplay project={project} key={index} />

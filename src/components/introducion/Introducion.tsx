67e04b92f46bde608b7eb36f0b36959b803c4b38
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 04:00:58 -0300

    feat: estruturação inicial da seção de introdução da página

--- src/components/introducion/Introducion.tsx
+++ src/components/introducion/Introducion.tsx
@@ -1,22 +1,27 @@
+import about_me from '../../data/about_me';
 import './Introducion.css';
 
 export default function Introducion() {
   return (
     <section id='introducion' className='introducion_container'>
-      <div className='introducion_container_content'>
-        <p>Prazer, meu nome é Guilherme Fernandes</p>
-        <ul>
-          <li>Nintendista</li>
-          <li>Leitor / Colecionador de livros</li>
-          <li>Wannabe photographer</li>
-        </ul>
-        <p>
-          Sou Desenvolvedor Web Full Stack, de Contagem em Minas Gerais.
-          Apaixonado por back-end e atualmente estudando Node.js, Typescript,
-          MongoDB e Python.
-        </p>
-        <button>Currículo</button>
+      <div>
+        <p>Prazer, meu nome é</p>
+        <p>Guilherme Fernandes</p>
       </div>
+      <ul>
+        {about_me.map((item, index) => (
+          <li key={index}>
+            <img src={item.imageUrl} alt={item.description} />
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+      <p>
+        Sou Desenvolvedor Web Full Stack, de Contagem em Minas Gerais.
+        Apaixonado por back-end e atualmente estudando Node.js, Typescript,
+        MongoDB e Python.
+      </p>
+      <button>Currículo</button>
     </section>
   );
 }

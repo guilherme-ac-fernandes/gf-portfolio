4e0f4fdfb4558d44bc43cdef794895e6a5dd1837
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 01:31:57 -0300

    feat: criação da seção de ferramentas

--- src/components/tools/Tools.tsx
+++ src/components/tools/Tools.tsx
@@ -1,14 +1,20 @@
+import tools from '../../data/tools';
 import SectionTitle from '../section_title/SectionTitle';
+import ToolsDisplay from '../tools_display/ToolsDisplay';
 import './Tools.css';
 
 export default function Tools() {
   return (
-    <section className='tools_container'>
+    <section id='tools' className='tools_container'>
       <SectionTitle
         title='Tecnologias'
         text='Estas são as linguagens e ferramentas que conheço'
       />
-      Tools
+      <div className='tools_container_display'>
+        {tools.map((tool, index) => (
+          <ToolsDisplay tool={tool} key={index} />
+        ))}
+      </div>
     </section>
   );
 }

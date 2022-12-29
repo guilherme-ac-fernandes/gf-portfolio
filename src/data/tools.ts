7dddbc6047d21caa794cfa2eef94dac9dcba6028
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 01:30:14 -0300

    feat: criação da lista de ferramentas

---
+++ src/data/tools.ts
@@ -0,0 +1,80 @@
+import Tools from '../interfaces/tools';
+
+const IMAGE_PATH = './tools';
+
+const tools: Tools[] = [
+  {
+    name: 'Git',
+    imageUrl: `${IMAGE_PATH}/git.png`,
+  },
+  {
+    name: 'VsCode',
+    imageUrl: `${IMAGE_PATH}/vscode.png`,
+  },
+  {
+    name: 'JavaScript',
+    imageUrl: `${IMAGE_PATH}/javascript.svg`,
+  },
+  {
+    name: 'TypeScript',
+    imageUrl: `${IMAGE_PATH}/typescript.svg`,
+  },
+  {
+    name: 'Python',
+    imageUrl: `${IMAGE_PATH}/python.png`,
+  },
+  {
+    name: 'HTML',
+    imageUrl: `${IMAGE_PATH}/html.svg`,
+  },
+  {
+    name: 'CSS',
+    imageUrl: `${IMAGE_PATH}/css.svg`,
+  },
+  {
+    name: 'React',
+    imageUrl: `${IMAGE_PATH}/react.svg`,
+  },
+  {
+    name: 'Redux',
+    imageUrl: `${IMAGE_PATH}/redux.png`,
+  },
+  {
+    name: 'Jest',
+    imageUrl: `${IMAGE_PATH}/jest.svg`,
+  },
+  {
+    name: 'RTL',
+    imageUrl: `${IMAGE_PATH}/rtl.png`,
+  },
+  {
+    name: 'Node.js',
+    imageUrl: `${IMAGE_PATH}/nodejs.svg`,
+  },
+  {
+    name: 'MySQL',
+    imageUrl: `${IMAGE_PATH}/mysql.svg`,
+  },
+  {
+    name: 'MongoDB',
+    imageUrl: `${IMAGE_PATH}/mongodb.svg`,
+  },
+  {
+    name: 'Express',
+    imageUrl: `${IMAGE_PATH}/express.svg`,
+  },
+  {
+    name: 'Mocha',
+    imageUrl: `${IMAGE_PATH}/mocha.png`,
+  },
+  {
+    name: 'Chai',
+    imageUrl: `${IMAGE_PATH}/chai.png`,
+  },
+  {
+    name: 'Sinon',
+    imageUrl: `${IMAGE_PATH}/sinon.png`,
+  },
+];
+
+export default tools;

789791a6723896a68ee504e6357d63351876ce59
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 03:24:13 -0300

    refactor: correção da página app após alterações de intro e about

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Introducion from './components/introducion/Intro';
+import Introducion from './components/introducion/Introducion';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
 import Tools from './components/tools/Tools';

2cdf52634054d66a954f0ecade3488167f7f580a
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-29 23:39:48 -0300

    feat: adição da animação na introdução

--- src/components/introducion/Introducion.tsx
+++ src/components/introducion/Introducion.tsx
@@ -1,27 +1,35 @@
-import about_me from '../../data/about_me';
+import { TypeAnimation } from 'react-type-animation';
 import './Introducion.css';
+
+const PHRASE = 'Eu programo';
+const SET_TIME = 2000;
 
 export default function Introducion() {
   return (
     <section id='introducion' className='introducion_container'>
       <div>
-        <p>Prazer, meu nome é</p>
+        <p>Oi, meu nome é</p>
         <p>Guilherme Fernandes</p>
+        <p>Desenvolvedor Web Full Stack</p>
+        <TypeAnimation
+          sequence={[
+            `${PHRASE} em JavaScript..`,
+            SET_TIME,
+            `${PHRASE} em TypeScript..`,
+            SET_TIME,
+            `${PHRASE} em Python..`,
+            SET_TIME,
+            `${PHRASE} para você!!`,
+            SET_TIME,
+          ]}
+          className='introducion_animation'
+          wrapper='div'
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: '1.8rem' }}
+        />
       </div>
-      <ul>
-        {about_me.map((item, index) => (
-          <li key={index}>
-            <img src={item.imageUrl} alt={item.description} />
-            <p>{item.description}</p>
-          </li>
-        ))}
-      </ul>
-      <p>
-        Sou Desenvolvedor Web Full Stack, de Contagem em Minas Gerais.
-        Apaixonado por back-end e atualmente estudando Node.js, Typescript,
-        MongoDB e Python.
-      </p>
-      <button>Currículo</button>
+      <button className='introducion_button'>Currículo</button>
     </section>
   );
 }

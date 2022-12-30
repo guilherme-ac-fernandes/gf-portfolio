bfd903306c53d48c593c9e8bc8ee0abda86cf487
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-30 01:10:12 -0300

    feat: criação do componente de contato

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,19 @@
+import SectionTitle from '../section_title/SectionTitle';
+
+import './Contact.css';
+
+export default function Contact() {
+  return (
+    <section id='contact' className='contact_container'>
+      <SectionTitle
+        title='Contato'
+        text='Envie uma mensagem para meu email através do formulário abaixo'
+      />
+      <form>
+        <input type='text' id='name' placeholder='Nome' />
+        <input type='text' id='email' placeholder='Email' />
+        <textarea id='message' placeholder='Digite sua mensagem...'></textarea>
+      </form>
+    </section>
+  );
+}

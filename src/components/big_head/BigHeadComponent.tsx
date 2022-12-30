ee103bc74a2d9636b1105c053187a18255df8dd9
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-30 00:15:16 -0300

    feat: criação do componente de bighead

---
+++ src/components/big_head/BigHeadComponent.tsx
@@ -0,0 +1,25 @@
+import { BigHead } from '@bigheads/core';
+import './BigHeadComponent.css';
+
+export default function BigHeadComponent() {
+  return (
+    <div className='bighead'>
+      <BigHead
+        accessory='roundGlasses'
+        body='chest'
+        circleColor='blue'
+        clothing='vneck'
+        clothingColor='white'
+        eyebrows='raised'
+        eyes='happy'
+        facialHair='mediumBeard'
+        graphic='react'
+        hair='buzz'
+        hairColor='black'
+        hat='none'
+        mouth='openSmile'
+        skinTone='brown'
+      />
+    </div>
+  );
+}

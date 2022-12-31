127011300df907fcfe83e1d8e12f8cc13ef6b4e1
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-30 23:11:03 -0300

    feat: adiciona referências

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ import Tools from './components/tools/Tools'
 import './App.css'
 import Contact from './components/contact/Contact'
 
+// Referências:
+// Portfólio construído baseado nos sequintes criações de colegas de profissão:
+// 1. Luá Octaviano (source: https://luaoctaviano.dev/)
+// 2. João Pedro Oliveira (source: https://jpoliveiramateus.vercel.app/)
+// 3. Ícaro Harry (source: https://icaro.io)
 function App() {
   return (
     <div>

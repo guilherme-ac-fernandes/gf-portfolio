1f434e524529a40823078bf57c3391849cc05e63
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-30 23:57:59 -0300

    feat: estruturação do botão de currículo

--- src/components/introducion/Introducion.tsx
+++ src/components/introducion/Introducion.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-alert */
 import React from 'react'
 
 import { TypeAnimation } from 'react-type-animation'
+import { FaFileDownload } from 'react-icons/fa'
 import BigHeadComponent from '../big_head/BigHeadComponent'
+
 import './Introducion.css'
 
 const PHRASE = 'Eu programo'
 const SET_TIME = 2000
 
 export default function Introducion() {
+  const handleResume = () => {
+    alert('Função não implementada')
+  }
+
   return (
     <section id="introducion" className="introducion_container">
       <BigHeadComponent />
@@ -32,8 +39,13 @@ export default function Introducion() {
           repeat={Infinity}
           style={{ fontSize: '1.3rem' }}
         />
-        <button className="introducion_button" type="button">
-          Adicionar algo
+        <button
+          onClick={handleResume}
+          className="introducion_button"
+          type="button"
+        >
+          Currículo
+          <FaFileDownload />
         </button>
       </div>
     </section>

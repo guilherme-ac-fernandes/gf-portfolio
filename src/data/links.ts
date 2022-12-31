cdccd023e74ae1d8304a05d72d31b60080b5586b
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-31 11:16:36 -0300

    feat: padronização dos links

---
+++ src/data/links.ts
@@ -0,0 +1,9 @@
+export const GITHUB_LINK = "https://github.com/guilherme-ac-fernandes";
+
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/guilherme-ac-fernandes/";
+
+export const MAILTO_EMAIL_LINK = "mailto:gfengquimica@gmail.com";
+
+export const EMAIL_LINK = "gfengquimica@gmail.com";
+
+export const RESUME_LINK = "";

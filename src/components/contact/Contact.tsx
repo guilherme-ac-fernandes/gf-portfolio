dc27976c8a55322af0e5438c563c02165bcc5c16
Guilherme Fernandes <gfengquimica@gmail.com>
2022-12-31 11:51:00 -0300

    feat: implementa os estados do formulário

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionTitle from '../section_title/SectionTitle'
 
 import './Contact.css'
 
 export default function Contact() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+
+  const handleSubmit = () => {
+    console.log(name, email, message)
+  }
+
   return (
     <section id="contact" className="contact_container">
       <SectionTitle
@@ -11,9 +19,28 @@ export default function Contact() {
         text="Envie uma mensagem para meu email através do formulário abaixo"
       />
       <form>
-        <input type="text" id="name" placeholder="Nome" />
-        <input type="text" id="email" placeholder="Email" />
-        <textarea id="message" placeholder="Digite sua mensagem..." />
+        <input
+          type="text"
+          id="name"
+          placeholder="Nome"
+          value={name}
+          onChange={({ target: { value } }) => setName(value)}
+        />
+        <input
+          type="text"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+        <textarea
+          id="message"
+          placeholder="Digite sua mensagem..."
+          value={message}
+          onChange={({ target: { value } }) => setMessage(value)}
+        />
+
+        <button onClick={handleSubmit} type="button">Enviar</button>
       </form>
     </section>
   )

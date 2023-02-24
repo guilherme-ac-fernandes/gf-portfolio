e667ddcb6b6e39c0bd2a0ace1ed9947f533d23cb
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2023-02-24 15:59:42 -0300

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -6,4 +6,4 @@ export const MAILTO_EMAIL_LINK = "mailto:gfengquimica@gmail.com";
 
 export const EMAIL_LINK = "gfengquimica@gmail.com";
 
-export const RESUME_LINK = "https://drive.google.com/file/d/1SsjssyJU0JF9HWY5xbL56b4w8_2zAzwh/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/17rNFov-fTcL4oiWNq5vjhiTd8D7ypPYL/view?usp=sharing";
